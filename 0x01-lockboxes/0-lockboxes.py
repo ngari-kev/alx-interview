5b82aad494d0ff8354b058e64c4b507b4ecd0568
ngari-kev <kevngariwangui@gmail.com>
2024-11-01 02:58:37 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Implementation of Lockboxes"""
 
+
 def canUnlockAll(boxes):
     """
     checks if all boxes can be opened
     Returns True if possible otherwise False
     """
-    
+
     unlocked = set([0])
     n = len(boxes)
     keys = set(boxes[0])
-    
+
     while keys:
         new_key = keys.pop()
         if 0 <= new_key < n and new_key not in unlocked:
